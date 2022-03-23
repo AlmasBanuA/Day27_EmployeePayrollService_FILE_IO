739aacd5bf247f0b46c00b4d801ea772ae12527a
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-03-23 19:32:21 +0530

    Ability to read the Employee Payroll File from the readData method

--- src/test/java/com/day27/employeepayroll/EmployeePayrollServiceTest.java
+++ src/test/java/com/day27/employeepayroll/EmployeePayrollServiceTest.java
@@ -21,9 +21,11 @@ public class EmployeePayrollServiceTest {
 		/**
 		 * created the array of employee Payroll data to store the entries
 		 */
-		EmployeePayrollData[] arrayOfEmp = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+		EmployeePayrollData[] arrayOfEmp = { 
+				new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
 				new EmployeePayrollData(2, "Bill Gates", 200000.0),
-				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) 
+			};
 		EmployeePayrollService employeePayrollService;
 		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
 
