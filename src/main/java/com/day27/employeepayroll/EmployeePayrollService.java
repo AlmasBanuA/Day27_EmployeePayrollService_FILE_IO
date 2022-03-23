d5f05421c82c9b2a575a23d4a785a5f1eb6f27c5
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-03-23 14:27:40 +0530

    Creates Employee Payroll Service Which reads and writes data to a console

--- src/main/java/com/day27/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/day27/employeepayroll/EmployeePayrollService.java
@@ -2,8 +2,8 @@ package com.day27.employeepayroll;
 
 /**
  * UC1:- Creating an Employee Payroll Service to Read and Write Employee Payroll to a Console 
- * 		 - Create Employee Payroll Class of id, name and Salary 
- * 		 - Read the information from the Console - Finally write the Employee Payroll to the Console
+ * - Create Employee Payroll Class of id, name and Salary 
+ * - Read the information from the Console - Finally write the Employee Payroll to the Console
  */
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +24,24 @@ public class EmployeePayrollService {
 	}
 
 	/**
-	 * creating List of Type EmployeePayrollData using ArrayList to store all the
-	 * employeePayrolldata
+	 * creating List of EmployeePayrollData
 	 */
-	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();;
+	private List<EmployeePayrollData> employeePayrollList;
 
 	/**
 	 * creating constructor of EmployeePayrollService() which is default type
 	 */
 	public EmployeePayrollService() {
-		employeePayrollList = new ArrayList<>();
+	}
+
+	/**
+	 * creating parameterized constructor of EmployeePayrollService with no return
+	 * type
+	 * 
+	 * @param employeePayrollList -passing parameter
+	 */
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
 	}
 
 	/**
@@ -43,9 +51,15 @@ public class EmployeePayrollService {
 	 */
 	public static void main(String[] args) {
 		/**
-		 * creating instance of EmployeePayrollService class
+		 * creating List of Type EmployeePayrollData using ArrayList to store all the
+		 * employeePayrolldata
 		 */
-		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+
+		/**
+		 * creating instance of EmployeePayrollData class
+		 */
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
 
 		/**
 		 * using scanner to read input from console
@@ -53,37 +67,38 @@ public class EmployeePayrollService {
 		Scanner consoleInputReader = new Scanner(System.in);
 
 		/**
-		 * Calling method readEmployeePayrollData to read data from console
+		 * Calling method readEmployeeData to read data from console
 		 */
-		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.readEmployeeData(consoleInputReader);
 
 		/**
 		 * Calling method writeEmployeePayrollData to write the data
 		 */
-		employeePayrollService.writeEmployeePayrollData();
-
+		employeePayrollService.writeEmployeeData();
 	}
 
 	/**
-	 * created method readEmployeePayrollData which reads the data of the employee
-	 * from the console
+	 * @param consoleInputReader Read employee data
 	 */
-	private void readEmployeePayrollData(Scanner consoleInputReader) {
-		System.out.println("Enter Employee ID: ");
-		int id = consoleInputReader.nextInt();
-		System.out.println("Enter Employee Name: ");
-		String name = consoleInputReader.next();
-		System.out.println("Enter Employee Salary: ");
-		double salary = consoleInputReader.nextDouble();
+	private void readEmployeeData(Scanner consoleInputReader) {
+		System.out.println("Enter employee ID : ");
+		int id = Integer.parseInt(consoleInputReader.nextLine());
+		System.out.println("Enter employee name : ");
+		String name = consoleInputReader.nextLine();
+		System.out.println("Enter employee salary : ");
+		double salary = Double.parseDouble(consoleInputReader.nextLine());
+
+		/**
+		 * Adding EmployeePayrollData to the employeepayrollList
+		 */
 		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 	}
 
 	/**
-	 * created method writeEmployeePayrollData which writes the data of the employee
-	 * in the console
+	 * created method writeEmployeeData which writes the data of the employee in the
+	 * console
 	 */
-	private void writeEmployeePayrollData() {
-		System.out.println("\nWriting Employee Payroll to console\n" + employeePayrollList);
+	private void writeEmployeeData() {
+		System.out.println("Writing Employee Payroll Data to Console\n" + employeePayrollList);
 	}
-
 }
